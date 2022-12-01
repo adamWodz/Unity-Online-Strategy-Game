4df6140e2053d4eb35defac55d35606e8471d823
MateuszFabisiewicz <01151578@pw.edu.pl>
2022-12-01 20:37:36 +0100

    added rolling mission panel

--- Assets/Scripts/PathsPanel.cs
+++ Assets/Scripts/PathsPanel.cs
@@ -2,14 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PathsPanel : MonoBehaviour
 {
-    
     public List<string> paths;
+    RectTransform scrollPanel;
+    float maxWidth = 611.61f;
+    float minWidth = 419.77f;
+    float scrollWidth;
+    float speed;
+    private PanelState panelState = PanelState.Maximized;
+    private Button button;
+    public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
+        //pobieram game managera
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+        //pobieram odpowiedni przycisk i nadajê mu funkcjê
+        button = GameObject.Find("DrawMissionsCardsButton").GetComponent<Button>();
+        button.onClick.AddListener(() => { gameManager.ChangeState(ref panelState,ref speed); });
+        
+        // pobieram RectTransform z PathsScroll, który jest rodzicem PathsPanel
+        scrollPanel = transform.parent.GetComponent<RectTransform>();
+        scrollWidth= scrollPanel.sizeDelta.x;
+        
         GameObject playerTextTempalte = transform.GetChild(0).gameObject;
         GameObject path;
 
@@ -27,6 +47,6 @@ public class PathsPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameManager.ChangeWidth(ref panelState, ref scrollWidth, ref scrollPanel, maxWidth,minWidth,ref speed); 
     }
 }
