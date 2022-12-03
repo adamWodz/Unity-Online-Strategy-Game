00b1039af1a0b8e456ce0ce522d7f3aad5059bd7
MateuszFabisiewicz <01151578@pw.edu.pl>
2022-12-03 16:51:04 +0100

    refactored code for panels

--- Assets/Scripts/PathsPanel.cs
+++ Assets/Scripts/PathsPanel.cs
@@ -4,31 +4,14 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class PathsPanel : MonoBehaviour
+public class PathsPanel : Panel
 {
     public List<string> paths;
-    RectTransform scrollPanel;
-    float maxWidth = 611.61f;
-    float minWidth = 419.77f;
-    float scrollWidth;
-    float speed;
-    private PanelState panelState = PanelState.Maximized;
-    private Button button;
-    public GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        //pobieram game managera
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        
-        //pobieram odpowiedni przycisk i nadaj� mu funkcj�
-        button = GameObject.Find("DrawMissionsCardsButton").GetComponent<Button>();
-        button.onClick.AddListener(() => { gameManager.ChangeState(ref panelState,ref speed); });
-        
-        // pobieram RectTransform z PathsScroll, kt�ry jest rodzicem PathsPanel
-        scrollPanel = transform.parent.GetComponent<RectTransform>();
-        scrollWidth= scrollPanel.sizeDelta.x;
+        AssignValues(419.77f, 611.61f, PanelState.Maximized, true);
         
         GameObject playerTextTempalte = transform.GetChild(0).gameObject;
         GameObject path;
@@ -47,6 +30,6 @@ public class PathsPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameManager.ChangeWidth(ref panelState, ref scrollWidth, ref scrollPanel, maxWidth,minWidth,ref speed); 
+        ChangeWidth(); 
     }
 }
