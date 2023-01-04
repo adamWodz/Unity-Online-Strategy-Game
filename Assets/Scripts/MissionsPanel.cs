1877d5f96c1ca4d4a6ff98e1180d10e9c774d39b
MateuszFabisiewicz <01151578@pw.edu.pl>
2023-01-04 13:11:06 +0100

    added two popUps
    - one if there is not enough mission cards to draw
    - one if player did not choose any mission card

--- Assets/Scripts/MissionsPanel.cs
+++ Assets/Scripts/MissionsPanel.cs
@@ -54,16 +54,23 @@ public class MissionsPanel : Panel
     
     private void DrawMissions()
     {
-        popUpPanel.SetActive(true);
-
-        var randomPaths = pathsPanel.GetRandomElementsFromList(missionsToChoose, 3);
-
-        Debug.Log($"RandomPaths: {randomPaths.Count}");
-        for (int i = 0; i < missionButtonsAndConfirmButton.Length - 1; i++)
+        if (missionsToChoose.Count < 3)
         {
-            int copy = i;
-            missionButtonsAndConfirmButton[copy].name = missionButtonsAndConfirmButton[copy].transform.GetChild(0).GetComponent<TMP_Text>().text = randomPaths[copy].start.name + "-" + randomPaths[copy].end.name;
-            missionButtonsAndConfirmButton[copy].onClick.AddListener(() => pathsPanel.HighlightPlanet(randomPaths[copy]));
+            SetPopUpWindow("Jest za ma³o kart misji, aby mo¿na by³o dobraæ nowe!");
+        }
+        else
+        {
+            popUpPanel.SetActive(true);
+
+            var randomPaths = pathsPanel.GetRandomElementsFromList(missionsToChoose, 3);
+
+            Debug.Log($"RandomPaths: {randomPaths.Count}");
+            for (int i = 0; i < missionButtonsAndConfirmButton.Length - 1; i++)
+            {
+                int copy = i;
+                missionButtonsAndConfirmButton[copy].name = missionButtonsAndConfirmButton[copy].transform.GetChild(0).GetComponent<TMP_Text>().text = randomPaths[copy].start.name + "-" + randomPaths[copy].end.name;
+                missionButtonsAndConfirmButton[copy].onClick.AddListener(() => pathsPanel.HighlightPlanet(randomPaths[copy]));
+            }
         }
 
     }
@@ -72,30 +79,44 @@ public class MissionsPanel : Panel
         // misje, które dobraliœmy
         var missionsChoosed = pathsPanel.missionsFromClickedMissionsCards.Except(pathsPanel.MissionsChoosed, new MissionComparer()).ToList();
         
-        pathsPanel.MissionsChoosed = missionsChoosed;
-        
-        // wygaszamy planety i przyciski
-        foreach(Mission m in missionsChoosed) 
+        if (missionsChoosed.Count > 0) // gracz musi dobraæ co najmniej jedn¹ kartê misji
         {
-            pathsPanel.HighlightPlanet(m);
-        }
+            pathsPanel.MissionsChoosed = missionsChoosed;
 
-        // "czyœcimy" przyciski z MissionsPanel
-        for (int i = 0; i < missionButtonsAndConfirmButton.Length - 1; i++)
+            // wygaszamy planety i przyciski
+            foreach (Mission m in missionsChoosed)
+            {
+                pathsPanel.HighlightPlanet(m);
+            }
+
+            // "czyœcimy" przyciski z MissionsPanel
+            for (int i = 0; i < missionButtonsAndConfirmButton.Length - 1; i++)
+            {
+                missionButtonsAndConfirmButton[i].onClick.RemoveAllListeners();
+                missionButtonsAndConfirmButton[i].name = "";
+            }
+
+            // usuwamy misje z listy misju mo¿liwych do wyboru i synchronizujemy t¹ listê z innymi graczami
+            foreach (Mission m in missionsChoosed)
+            {
+                SyncMissionsToChooseServerRpc(m.start.name, m.end.name);
+            }
+
+            // przywracamy stan paneli sprzed dobierania
+            ChangeState();
+            pathsPanel.ChangeState();
+            drawMissionsCardsButton.enabled = true;
+        }
+        else
         {
-            missionButtonsAndConfirmButton[i].onClick.RemoveAllListeners();
-            missionButtonsAndConfirmButton[i].name = "";
+            SetPopUpWindow("Musisz wybraæ co najmniej jedn¹ kartê misji!");
         }
+    }
 
-        // usuwamy misje z listy misju mo¿liwych do wyboru i synchronizujemy t¹ listê z innymi graczami
-        foreach(Mission m in missionsChoosed)
-        {
-            SyncMissionsToChooseServerRpc(m.start.name, m.end.name);
-        }
-
-        // przywracamy stan paneli sprzed dobierania
-        ChangeState();
-        pathsPanel.ChangeState();
-        drawMissionsCardsButton.enabled = true;
+    void SetPopUpWindow(string message)
+    {
+        var popUp = transform.parent.GetChild(0);
+        popUp.GetChild(0).GetComponent<TMP_Text>().text = message;
+        popUp.gameObject.SetActive(true);
     }
 }
