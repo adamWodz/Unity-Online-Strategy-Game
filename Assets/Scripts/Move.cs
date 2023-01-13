23b977c637325dabb272b1efdb1f435ab8b3ce74
MateuszFabisiewicz <01151578@pw.edu.pl>
2023-01-13 01:12:53 +0100

    added rest of new prefabs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,12 +12,6 @@ public class Move : MonoBehaviour
     public Vector3 goalPosition;
     public Quaternion goalRotation;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
     // Update is called once per frame
     void Update()
     {
